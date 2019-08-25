61a429ed3b809a238cd13071e30d420cde6df379
ASugumaran <suguanbu321@gmail.com>
2019-08-25 13:10:53 +0530

    Doubly Circular linked list

---
+++ Doubly_circular_linked_list/Doubly_circular_linked_list.c
@@ -0,0 +1,76 @@
+/****************************************************/
+/* Author      :     A.Sugumaran                    */
+/* Date        :     23-08-2019                     */
+/* Filename    :     Doubly_Circular_linked_list.c  */
+/* Description :     Doubly_Circular_linked_list    */
+/****************************************************/
+
+//Including header file
+#include"Doubly_circular_linked_list.h"
+
+/*Main structure*/
+struct node
+{
+	struct node *prev;
+	int num;
+	char *name;
+	struct node *next;
+};
+
+/*Assigning head = NULL */
+struct node *head = NULL;
+int main()
+{
+
+	int i=0;
+	/*Inserting first value*/
+	struct node *ptr1 = (struct node *)malloc(sizeof(struct node ));
+
+	ptr1->num = 1;
+	ptr1->name = "sugu";
+	ptr1->prev = NULL;
+	ptr1->next = NULL;
+
+	/*Inserting second value*/
+	struct node *ptr2 = (struct node *)malloc(sizeof(struct node));
+	ptr2->num = 2;
+	ptr2->name = "maran";
+	ptr2->prev = ptr1;
+	ptr2->next = NULL;
+
+	/*It will point that 2nd value after printed the 1st value*/
+	ptr1->next = ptr2;
+	ptr2->next = ptr1;
+
+	/*It will point to a 1st value*/
+	head = ptr1;
+
+	printf("Before Deletion\n");
+
+	/*Assigning to start from 1st value*/
+	struct node *print_ptr = head;
+	
+       /*Printing loop*/	
+	while(print_ptr != NULL)
+	{
+		i++;
+		printf("Node %d Num %d Name %s\n",i,print_ptr->num,print_ptr->name);
+		print_ptr = print_ptr->next;
+	}
+
+	/*It will point that only 1st value*/
+	ptr1->next = ptr1;
+	
+	printf("After deletion\n");
+	/*One more time need to assign because print_ptr incremented before in printing loop*/
+	print_ptr = head;	
+	i = 0;
+       /*Printing loop*/	
+	while(print_ptr != NULL)
+	{
+		i++;
+		printf("Node %d Num %d Name %s\n",i,print_ptr->num,print_ptr->name);
+		print_ptr = print_ptr->next;
+	}
+	
+}
---
+++ Doubly_circular_linked_list/Doubly_circular_linked_list.h
@@ -0,0 +1,21 @@
+/******************************************************/
+/* Author      :     A.Sugumaran                      */
+/* Date        :     23-08-2019                       */
+/* Filename    :     Doubly_Circular_linked_list.h    */
+/* Description :     Double Circular linked list      */
+/******************************************************/
+
+/* Included header file */
+#ifndef DOUBLY_CIRCULAR_LINKED_LIST_H
+#define DOUBLY_CIRCULAR_LINKED_LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Included all header files*/
+
+/*Macro definitons*/
+
+/*User defined datatype definitions*/
+
+#endif //DOUBLY_CIRCULAR_LINKED_LIST_H
