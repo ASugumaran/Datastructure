8c5983a3fd0b1569e0b6887fb784ac07d9348e1e
ASugumaran <suguanbu321@gmail.com>
2019-08-25 13:09:34 +0530

    Double linked list

---
+++ Double_linked_list/Double_linked_list.h
@@ -0,0 +1,21 @@
+/************************************************/
+/* Author      :     A.Sugumaran                */
+/* Date        :     17-08-2019                 */
+/* Filename    :     Double_linked_list.h       */
+/* Description :     Double linked list         */
+/************************************************/
+
+/* Included header file */
+#ifndef DOUBLE_LINKED_LIST_H
+#define DOUBLE_LINKED_LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Included all header files*/
+
+/*Macro definitons*/
+
+/*User defined datatype definitions*/
+
+#endif //DOUBLE_LINKED_LIST_H
