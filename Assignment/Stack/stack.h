cd597c480b4c8e67471538edb4253bf564f73baa
ASugumaran <suguanbu321@gmail.com>
2020-05-15 18:08:55 +0530

    Stack/

---
+++ Assignment/Stack/stack.h
@@ -0,0 +1,22 @@
+/***********************************************************/
+/*  Author       : Sugumaran_A                             */
+/*  File_Name    : Stack.h                                 */         
+/*  Description  : stack                                   */
+/*  Date         : 15-05-2020                              */
+/***********************************************************/
+
+#ifndef MAKE_H
+#define MAKE_H
+
+/* Included header files */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Macro Definitions */
+
+/* Functions prototype */
+/* user defined datatype definitions */
+
+
+#endif //MAKE_H
