dfd0ddb77194d5ce6f13363e765625e9c43acab6
ASugumaran <suguanbu321@gmail.com>
2020-05-16 16:58:03 +0530

    Queue

---
+++ Assignment/Queue/ReadMe.txt
@@ -0,0 +1,4 @@
+////////Queue//////
+Insert data into a Queue
+Delete data from a Queue
+Display data in a Queue
---
+++ Assignment/Queue/queue.h
@@ -0,0 +1,21 @@
+/***********************************************************/
+/*  Author       : Sugumaran_A                             */
+/*  File_Name    : queue.h                                 */
+/*  Description  : queue                                   */
+/*  Date         : 15-05-2020                              */
+/***********************************************************/
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Include all header files*/
+
+/*Macro definition*/
+
+/*User defined datatype*/
+
+#endif //QUEUE_H
+
