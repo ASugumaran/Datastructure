58331942ce5a5ce80c1e0714022bd6465c6819f7
ASugumaran <suguanbu321@gmail.com>
2020-05-18 16:21:04 +0530

    Sparse_Matrix/

---
+++ Assignment/Sparse_Matrix/ReadMe.txt
@@ -0,0 +1,1 @@
+A Sparse matrix or sparse array is a matrix in which most of the elements are zero.
---
+++ Assignment/Sparse_Matrix/sparse_matrix.h
@@ -0,0 +1,20 @@
+/***********************************************************/
+/*  Author       : Sugumaran_A                             */
+/*  File_Name    : Sparse martix.h                         */         
+/*  Description  : Sparse matrix                           */
+/*  Date         : 18-05-2020                              */
+/***********************************************************/
+
+#ifndef SPARSE_MATRIX_H
+#define SPARSE_MATRIX_H
+
+
+/*Include all header files*/
+
+#include <stdio.h>
+#include <stdlib.h>
+/*Macro definition*/
+
+/*User defined datatype*/
+
+#endif //SPARSE_MATRIX_H
